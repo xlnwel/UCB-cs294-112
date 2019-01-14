db14684f2d6083c06f6d593f650a1553a5639e30
xlnwel <122134545@qq.com>
2019-01-14 21:16:50 +0800

    Complete

--- hw2/run_hw.sh
+++ hw2/run_hw.sh
@@ -14,4 +14,4 @@ source activate cs294-112
 
 # python train_pg_f18.py InvertedPendulum-v2 -ep 1000 --discount 0.9 -n 100 -e 3 -l 2 -s 64 -b 1000 -lr 5e-3 -rtg --exp_name hc_b1e3_r5e-3
 
-python train_pg_f18.py LunarLanderContinuous-v2 -ep 1000 --discount 0.99 -n 100 -e 3 -l 2 -s 64 -b 40000 -lr 0.005 -rtg --nn_baseline --exp_name ll_b40000_r0.005
+python train_pg_f18.py LunarLanderContinuous-v2 -ep 1000 --discount 0.99 -n 100 -e 3 -l 2 -s 64 -b 40000 -lr 0.005 -rtg --nn_baseline --exp_name mse-logexp
