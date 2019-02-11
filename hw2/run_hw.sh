40b2302478c29bbbe5c2511d7a4a8be06fd097e6
xlnwel <122134545@qq.com>
2019-02-11 15:31:07 +0800

    Update run_hw.sh

--- hw2/run_hw.sh
+++ hw2/run_hw.sh
@@ -4,7 +4,7 @@ source activate cs294-112
 
 # python train_pg_f18.py CartPole-v0 -n 100 -b 1000 -e 3 -rtg -dna --exp_name sb_rtg_dna &
 
-# python train_pg_f18.py CartPole-v0 -n 100 -b 1000 -e 3 -rtg --exp_name sb_rtg_na &
+python train_pg_f18.py CartPole-v0 -n 100 -b 1000 -e 3 -rtg --exp_name sb_rtg_na
 
 # python train_pg_f18.py CartPole-v0 -n 100 -b 5000 -e 3 -dna --exp_name lb_no_rtg_dna &
 
@@ -14,4 +14,4 @@ source activate cs294-112
 
 # python train_pg_f18.py InvertedPendulum-v2 -ep 1000 --discount 0.9 -n 100 -e 3 -l 2 -s 64 -b 1000 -lr 5e-3 -rtg --exp_name hc_b1e3_r5e-3
 
-python train_pg_f18.py LunarLanderContinuous-v2 -ep 1000 --discount 0.99 -n 100 -e 3 -l 2 -s 64 -b 40000 -lr 0.005 -rtg --nn_baseline --exp_name mse-logexp
+# python train_pg_f18.py LunarLanderContinuous-v2 -ep 1000 --discount 0.99 -n 100 -e 3 -l 2 -s 64 -b 40000 -lr 0.005 -rtg --nn_baseline --exp_name mse-logexp
